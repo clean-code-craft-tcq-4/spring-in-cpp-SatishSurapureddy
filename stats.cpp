45d99fe27149a55d4038d5287a03dc539e5beeec
SatishSurapureddy <39698568+SatishSurapureddy@users.noreply.github.com>
2022-05-22 08:15:33 +0530

    Update stats.cpp
    added min, max and average to computeStatistics

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,13 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>&vec ) {
     //Implement statistics here
+    auto computedStats.min = *min_element(vec.begin(), vec.end());
+    auto computedStats.max = *max_element(vec.begin(), vec.end());
+    auto average = 0.0;
+    for(int i=0; i<vec.size(); ++i){
+     average = average + vec[i];   
+    }
+    auto computedStats.average = average/vec.size();
+    return computedStats;
 }
